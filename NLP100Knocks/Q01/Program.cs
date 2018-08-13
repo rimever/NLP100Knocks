5b2d6e832ee904fa42a67d865b87d4d6f926fbb5
RYOHEIMIURAD197\rim <mot_siva_ordin@hotmail.com>
2018-08-13 21:12:44 +0900

    Q00,Q01を解いたのでコミット。

---
+++ NLP100Knocks/Q01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q01
+{
+    class Program
+    {
+        /// <summary>
+        /// 01. 「パタトクカシーー」
+        /// 「パタトクカシーー」という文字列の1,3,5,7文字目を取り出して連結した文字列を得よ
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string text = "パタトクカシーー";
+            int[] index = new[] {1, 3, 5, 7};
+            StringBuilder result = new StringBuilder();
+            foreach (var item in index)
+            {
+                result.Append(text[item - 1]);
+            }
+            Console.WriteLine(result.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Q00/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q00
+{
+    class Program
+    {
+        /// <summary>
+        /// 00. 文字列の逆順
+        /// 文字列"stressed"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string text = "stressed";
+            Console.WriteLine(string.Join("",text.Reverse()));
+            Console.ReadKey();
+        }
+    }
+}
