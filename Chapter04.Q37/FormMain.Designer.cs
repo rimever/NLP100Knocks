a5b82ccc4dea748b32dd70321a9b0f993bb644d9
RYOHEIMIURAD197\rim <mot_siva_ordin@hotmail.com>
2018-08-29 17:31:30 +0900

    Q37を回答。 グラフなので新しくプロジェクトとして追加して実装をしてみた。

---
+++ Chapter04.Q37/FormMain.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Chapter04.Q37
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            this.chart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(0, 0);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(998, 729);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart1";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 729);
+            this.Controls.Add(this.chart);
+            this.Font = new System.Drawing.Font("Meiryo UI", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+    }
+}
+
---
+++ Chapter04.Q37/FormMain.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using Chapter04.Core;
+
+namespace Chapter04.Q37
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            Load += FormMain_Load;
+        }
+        private readonly MorphologicalAnalyzer _analyzer = new MorphologicalAnalyzer();
+
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+            _analyzer.Execute();
+            IDictionary<string, List<Word>> result = new Dictionary<string, List<Word>>();
+            foreach (var word in _analyzer.EnumerableWords())
+            {
+                if (!result.ContainsKey(word.Base))
+                {
+                    result.Add(word.Base, new List<Word>());
+                }
+                result[word.Base].Add(word);
+            }
+
+            ChartArea chartArea = new ChartArea("base")
+            {
+                AxisX = {Title = "単語"},
+                AxisY = {Title = "頻出回数"}
+            };
+
+            Series series = new Series
+            {
+                ChartType = SeriesChartType.Line,
+                Name = "単語の頻出回数",                
+                XValueType = ChartValueType.String,
+                ChartArea = chartArea.Name
+            };
+            chartArea.AxisX.MajorGrid.Interval = 1;
+            chartArea.AxisX.LabelStyle = new LabelStyle()
+            {
+                Interval = 1
+            };
+            chart.Series.Clear();
+            chart.ChartAreas.Clear();
+            chart.ChartAreas.Add(chartArea);
+            foreach (var item in result.OrderByDescending(pair => pair.Value.Count).Take(10).Select((value, index) => new {value, index}))
+            {
+                var first = item.value.Value.FirstOrDefault();
+                series.Points.AddXY(first.Base, item.value.Value.Count);
+            }
+
+            chart.Series.Add(series);
+        }
+    }
+}
